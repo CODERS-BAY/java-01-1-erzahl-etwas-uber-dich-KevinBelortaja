9dd4deec34ea5ecc1030774807e1491d1dd5a5e2
Kevin Belortaja <kevin.belortaja@gmail.com>
2021-02-11 13:34:27 +0100

    Add exercise

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        int age= 18;
-        String firstName= "Kevin";
-        char gender= 'm';
-        String lastName= "Belortaja";
-        Date birthday= new Date (1033596000000L);
-        double averageGrade= 1;
-        boolean married= false;
+        int age = 18;
+        String firstName = "Kevin";
+        char gender = 'm';
+        String lastName = "Belortaja";
+        Date birthday = new Date(1033596000000L);
+        double averageGrade = 1;
+        boolean married = false;
 
-        System.out.println("Age: "+age+", "+"First Name: "+firstName+", "+"Gender: "+gender+", "+ "Last Name: "+ lastName+", "+"Birthday: "+ birthday+", "+"Average Grade: " +averageGrade+", "+"Married: "+ married);
+        System.out.println("Age: " + age + ", " + "First Name: " + firstName + ", " + "Gender: " + gender + ", " + "Last Name: " + lastName + ", " + "Birthday: " + birthday + ", " + "Average Grade: " + averageGrade + ", " + "Married: " + married);
     }
 }
